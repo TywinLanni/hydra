b3d9d5b938f8d074441cfeaa2841a56bfcac6f63
Tywin <sasha291096@gmail.com>
2024-06-01 15:24:38 +0300

    Add module m1l1-first Add hello world Add first test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") apply false
 }
 
 group = "com.github.tywinlanni"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
 }
